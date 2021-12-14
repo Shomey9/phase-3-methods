02b91d8a550cf3f690fac043d3fc6e81ac09d3fc
Muhammad Rendusara <hadyanrendusara@yahoo.com>
2021-12-14 07:17:12 -0600

    commit tree ecb6fb703af5c79354ba4c58a2b0917ad2b5ffc9

--- methods.rb
+++ methods.rb
@@ -1,1 +1,27 @@
-# Your code here!
+def greet_programmer 
+  puts "Hello, programmer!"
+end
+
+def greet name
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default name = "programmer"
+    if name.class === "String" 
+        puts "Hello, #{name}!"
+    end
+end
+
+def add num1, num2
+    num1 + num2
+end
+
+def halve num
+    if num.class == Integer
+        num / 2
+    else
+    end
+    
+end
+
+
